60818e5a956f6dc3eda2d561ebba0c38e5e33f9b
sergiocpxfontes <79989206+sergiocpxfontes@users.noreply.github.com>
2022-03-17 19:49:54 +0000

    Update tests_api.py
    Correção ao teste test_tarefas_getall

--- tests_api.py
+++ tests_api.py
@@ -1,12 +1,12 @@
 import unittest
-from webapi.tarefascls import Tarefa
+from webapi.tarefascls import Tarefa,TarefaOps
 from webapi import views as webapiviews
 
 import xmlrunner
 
 class TestWebApp(unittest.TestCase):
     def test_tarefas_getall(self):
-        resultado = Tarefa.GetAll()
+        resultado = TarefaOps.GetAll()
         self.assertNotEqual(resultado,0)
     
     def test_api_tarefas_getall(self):
