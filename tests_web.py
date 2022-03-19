dd2fc7b94101aa9813bec4e145d0a521f3e905c7
sergiocpxfontes <79989206+sergiocpxfontes@users.noreply.github.com>
2022-03-19 09:48:19 +0000

    Update tests_web.py
    Trigger jenkins

--- tests_web.py
+++ tests_web.py
@@ -13,7 +13,6 @@ class TestWebApp(unittest.TestCase):
         rv = self.app.get('/home')
         #self.assertIn('Sergio', str(rv.data))
         self.assertIn('My Flask Application', str(rv.data))
-
 if __name__ =="__main__":
     unittest.main()
     #unittest.main(testRunner = xmlrunner.XMLTestRunner(output='relatorio-testes'))
